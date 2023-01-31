a5d6b5c700bcb44b108b619992edde6293f96762
JuanLuisCoutiño <introspectivemotion@gmail.com>
2023-01-31 15:47:33 -0600

    Initial commit

---
+++ .idea/scratch_7.kts
@@ -0,0 +1,7 @@
+import javax.swing.JOptionPane
+
+println("Vamos a mostrar un dialogo")
+
+var numero= JOptionPane.showInputDialog("Ingresa tu turno")!!.toInt()
+numero++
+JOptionPane.showMessageDialog(null, "Ese es tu turno: ${numero}")
