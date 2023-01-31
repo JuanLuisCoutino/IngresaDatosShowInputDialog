a8460ba5fdd78a1202cd429e7d92eacf6a25372a
JuanLuisCoutino <123596662+JuanLuisCoutino@users.noreply.github.com>
2023-01-31 17:04:26 -0600

    Update scratch_7.kts

--- .idea/scratch_7.kts
+++ .idea/scratch_7.kts
@@ -5,3 +5,5 @@ println("Vamos a mostrar un dialogo")
 var numero= JOptionPane.showInputDialog("Ingresa tu turno")!!.toInt()
 numero++
 JOptionPane.showMessageDialog(null, "Ese es tu turno: ${numero}")
+
+//Ing. JUAN LUIS COUTIÑO LÓPEZ
